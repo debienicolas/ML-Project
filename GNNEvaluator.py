98fae1c9b999c2011f25a8041586d7f40b347c7a
Nicolas De Bie <nicolas.debie.nd@gmail.com>
2023-05-04 17:58:03 +0200

    changes

--- GNNEvaluator.py
+++ GNNEvaluator.py
@@ -4,10 +4,9 @@ import Graph
 import numpy as np
 
 class GNNEvaluator(Evaluator):
-    def __init__(self, game, nnet, args):
+    def __init__(self, nnet, args):
         self.nnet = nnet
         self.args = args
-        self.game = game
 
     def evaluate(self, state):
         """Returns evaluation on given state."""
@@ -15,18 +14,17 @@ class GNNEvaluator(Evaluator):
         #print("Evaluation: ", np.array([v,-v]))
         return np.array([v,-v])
         
-    
 
     def prior(self, state):
         """Returns equal probability for all actions."""
         if state.is_chance_node():
             return state.chance_outcomes()
         else:
-            pi, _ = self.nnet.predict(state)
+            pi, _ = self.nnet.predict(state=state)
             # the pi received from predict is a list of probabilities for each action
-            pi = Graph.edges_to_actions(self.game,pi.tolist())
+            pi = Graph.edges_to_actions(state,pi.tolist())
             #print("Pi: ", pi)
-            assert len(Graph.edges_to_actions(self.game,pi)) == state.num_distinct_actions()
+            assert len(Graph.edges_to_actions(state,pi)) == state.num_distinct_actions()
             # Still need to filter out the valid actions
             result = [(action,pi[action]) for action,val in enumerate(state.legal_actions_mask()) if val]
             # filter out not none probabilities
